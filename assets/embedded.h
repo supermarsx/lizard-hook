0ff65322f7c0b59a353458c217d309d866d71222
Mariana <github_618a@imariana.com>
2025-08-18 00:14:12 +0100

    Reuse existing PNG for macOS tray

--- assets/embedded.h
+++ assets/embedded.h
@@ -5,4 +5,4 @@ extern const unsigned char lizard_regular_png[];
 extern const unsigned int lizard_regular_png_len;
 extern const unsigned char lizard_processed_clean_no_meta_flac[];
 extern const unsigned int lizard_processed_clean_no_meta_flac_len;
-}
+} // namespace lizard::assets
